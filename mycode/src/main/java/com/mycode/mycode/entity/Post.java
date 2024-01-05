7f97f3e690126b00cf70bff482b3a96bbee415c3
suhana30 <suhanakhang30@gmail.com>
2024-01-05 16:37:54 +0530

    127:create save Post Feature Entity class added

---
+++ mycode/src/main/java/com/mycode/mycode/entity/Post.java
@@ -0,0 +1,25 @@
+package com.mycode.mycode.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Post {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    private String content;
+    private String title;
+
+}
